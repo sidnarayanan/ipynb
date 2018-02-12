26a77460f567deb1e97eab96c3de5575ae8ef75c
Sid Narayanan <sidn@mit.edu>
2018-02-11 21:44:57 -0500

    initial commit

---
+++ data/get_phedexerrors.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from os import system 
+import json 
+
+#system('mkdir jsons; cat nodes.txt | xargs -P5 -n1 -I {} wget -O jsons/{}.json --no-check-certificate "https://cmsweb.cern.ch/phedex/datasvc/json/prod/errorlog?from={}"')
